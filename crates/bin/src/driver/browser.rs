c4a8eaee46626d9d9a5cc398020af203bdf9dbbd
shivaraj-bh <me@shivaraj-bh.in>
2026-06-06 14:01:51 +0530

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:crates/bin/src/driver/browser.rs

--- crates/bin/src/driver/browser.rs
+++ crates/bin/src/driver/browser.rs
@@ -48,36 +48,6 @@ impl BrowserDriver {
     ) -> SharedBoxedObservable<'static, BrowserSpawnEvent, Infallible> {
         todo!()
     }
-
-    pub(crate) fn new() -> (
-        SharedBoxedObservable<'static, BrowserEvent, Infallible>,
-        Self,
-    ) {
-        let (event_sender, event_receiver) = tokio::sync::mpsc::channel(1);
-        let driver = Self { event_sender };
-        (
-            Shared::from_stream(ReceiverStream::new(event_receiver)).box_it(),
-            driver,
-        )
-    }
-
-    pub(crate) fn effect(&self, command: BrowserCommand) -> impl Future<Output = ()> + 'static {
-        let event_sender = self.event_sender.clone();
-
-        async move {
-            let event = match command {
-                BrowserCommand::Spawn { url: address } => match Browser::spawn(address).await {
-                    Ok(browser) => BrowserEvent::Spawn(browser),
-                    Err(error) => BrowserEvent::SpawnError(error),
-                },
-                BrowserCommand::Reload(browser) => match browser.reload().await {
-                    Ok(_) => BrowserEvent::Reload(browser),
-                    Err(error) => BrowserEvent::ReloadError(browser, error),
-                },
-            };
-            event_sender.send(event).await.unwrap();
-        }
-    }
 }
 
 #[derive(Debug)]
